5c24e42fce4ca8bbddeb55c062682eca9ced90ad
devboell <devboell@gmail.com>
2018-10-19 09:55:07 +0200

    removed unneeded styling

--- src/client/components/HomeIconArea/HomeLink/index.js
+++ src/client/components/HomeIconArea/HomeLink/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 import homeImage from './images/home.png'
 import Image from './Image'
-import Wrapper from './Wrapper'
 
 const HomeLink = () =>
-  <Wrapper>
-    <Link to="/" >
-      <Image src={homeImage} alt="Home" />
-    </Link>
-  </Wrapper>
+  <NavLink to="/" >
+    <Image src={homeImage} alt="Home" />
+  </NavLink>
 
 export default HomeLink
