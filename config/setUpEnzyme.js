c1db61712eb831ed43a8a3df38caf429483aa0ee
devboell <devboell@gmail.com>
2018-10-19 08:52:30 +0200

    comment out enzyme

--- config/setUpEnzyme.js
+++ config/setUpEnzyme.js
@@ -1,11 +1,12 @@
 // import 'babel-polyfill'
 // import 'raf/polyfill'
 
-import { shallow, render, mount, configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16' // eslint-disable-line import/no-extraneous-dependencies
+// import { shallow, render, mount, configure } from 'enzyme'
+// import Adapter from 'enzyme-adapter-react-16'
+// eslint-disable-line import/no-extraneous-dependencies
 
-configure({ adapter: new Adapter() })
+// configure({ adapter: new Adapter() })
 
-global.shallow = shallow
-global.render = render
-global.mount = mount
+// global.shallow = shallow
+// global.render = render
+// global.mount = mount
